ef456a84388896d049ef37807f6946725835ac5e
Robson Rodrigues Ribeiro <projetos.r@hotmail.com>
2022-08-12 23:26:34 -0300

    atualizando package json

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 /**************************** IMPORTS ******************************/
 import express, { Express } from "express"
+import { AddressInfo } from "net";
 import cors from "cors"
 import { userRouter } from "./routers/userRouter"
 import { postRouter } from "./routers/postRouter"
 import { amizadeRouter } from "./routers/AmizadeRoute"
 import DataAcess from "./data/DataAcess"
 import { pacienteRouter } from "./routers/PacienteRoute"
-import { AddressInfo } from "net";
+
 
 /**************************** CONFIG ******************************/
 const app: Express = express()
@@ -21,11 +22,11 @@ app.use('/post', postRouter )
 app.use('/amizade', amizadeRouter )
 app.use('/paciente', pacienteRouter )
 /**************************** SERVER INIT ******************************/
-const server = app.listen(process.env.PORT || 3003, () => {
-   if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://${address.address}:${address.port}`);
-   } else {
-       console.error(`Failure upon starting server.`);
-   }
-});
+const server = app.listen(process.env.PORT || 3003, ()=>{
+    if(server){
+        const address = server.address() as AddressInfo
+        console.log(`Server is running in http://localhos:${address.port}`)
+    }else{
+        console.log(`Failure upon starting server. `)
+    }
+})
